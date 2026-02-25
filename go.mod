e1bc5418874f2d142d4c22ab5225ad4fd065b9ca
allofthenamesaretaken <abauwmans@gmail.com>
2026-02-25 12:04:38 -0500

    feat(dotenv): added dotenv file config support

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/allofthenamesaretaken/anytype-exporter
 
 go 1.25.3
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/allofthenamesaretaken/anytype-exporter/utils"
+	"github.com/joho/godotenv"
 )
 
 func initLogger() *utils.Logger {
@@ -20,5 +23,19 @@ func main() {
 	logInstance := initLogger()
 	defer logInstance.Close()
 
+	// dotenv vars
+	err := godotenv.Load()
+	if err != nil {
+		logInstance.Error("Failed to load .env file", err)
+	}
+
+	anytypeBaseUrl := os.Getenv("ANYTYPE_BASE_URL")
+	anytypeApiKey := os.Getenv("ANYTYPE_API_KEY")
+	anytypeVersion := os.Getenv("ANYTYPE_VERSION")
+	exportDir := os.Getenv("EXPORT_DIR")
+	fmt.Printf("%v\n", anytypeBaseUrl)
+	fmt.Printf("%v\n", anytypeApiKey)
+	fmt.Printf("%v\n", anytypeVersion)
+	fmt.Printf("%v\n", exportDir)
 	print("hello world")
 }
